236ace01b26de5e314ceb73997bb7a30626503cd
Ahsan Ullah Siddiquee <ahsansk84@hotmail.com>
2024-08-23 09:41:07 +0600

    Catalog

---
+++ scr/Services/Catalog/CatalogAPI/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,42 @@
+﻿namespace CatalogAPI.Products.CreateProduct
+{
+    public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
+        :ICommand<CreateProductResult>;
+    public record CreateProductResult(Guid Id);
+
+    internal class CreateProductCommandHandler
+        (IDocumentSession session, ILogger<CreateProductCommandHandler> logger)
+        : ICommandHandler<CreateProductCommand, CreateProductResult>
+    {
+        public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+        {
+            //create Poduct entity from command objet
+            //save to database
+            //return CreateProductResult result
+
+            logger.LogInformation("CreateProductCommandHandler.Handel called with {command}", command);
+
+            var product = new Product
+            {
+                Name = command.Name,
+                Category = command.Category,
+                Description = command.Description,
+                ImageFile = command.ImageFile,
+                Price = command.Price
+            };
+
+            //TODO
+            //save to databas
+            if (product != null && session != null)
+            {
+                session.Store(product);
+                await session.SaveChangesAsync(cancellationToken);
+            }
+
+            //return result
+
+            return new CreateProductResult(Guid.NewGuid());
+
+        }
+    }
+}
---
+++ scr/Services/Catalog/CatalogAPI/Products/DeleteProduct/GetProductByIdHandler.cs
@@ -0,0 +1,23 @@
+﻿
+namespace CatalogAPI.Products.DeleteProduct
+{
+    public record DeleteProductCommmand(Guid Id): IQuery<DeleteProductResult>;
+
+    public record DeleteProductResult(bool IsSuccess);
+
+
+    internal class DeleteProductCommmandHandler
+        (IDocumentSession session, ILogger<DeleteProductCommmandHandler> logger)
+        : IQueryHandler<DeleteProductCommmand, DeleteProductResult>
+    {
+        public async Task<DeleteProductResult> Handle(DeleteProductCommmand commmand, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("DeleteProductCommmandHandler.Handel called with {query}", commmand);
+
+            session.Delete<Product>(commmand.Id);
+            await session.SaveChangesAsync(cancellationToken);
+
+            return new DeleteProductResult(true);
+        }
+    }
+}
