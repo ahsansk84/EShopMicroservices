42cb1d748ac8e15e47ba38e79dcc79ac182657d9
Ahsan Ullah Siddiquee <ahsansk84@hotmail.com>
2024-10-08 12:17:58 +0600

    Ordering project

---
+++ scr/Services/Ordering/Ordering.Infrastructure/Data/Configurations/OrderConfiguration.cs
@@ -0,0 +1,119 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Ordering.Domain.Enums;
+
+namespace Ordering.Infrastructure.Data.Configurations
+{
+    public class OrderConfiguration : IEntityTypeConfiguration<Order>
+    {
+        public void Configure(EntityTypeBuilder<Order> builder)
+        {
+            builder.HasKey(o => o.Id);
+            builder.Property(o => o.Id).HasConversion(
+                orderId => orderId.Value,
+                dbId => OrderId.Of(dbId));
+            builder.HasOne<Customer>()
+                .WithMany()
+                .HasForeignKey(o => o.CustomerId)
+                .IsRequired();
+            builder.HasMany(o => o.OrderItems)
+                .WithOne()
+                .HasForeignKey(oi => oi.OrderId);
+            builder.ComplexProperty(
+                o => o.OrderName, nameBuilder =>
+                {
+                    nameBuilder.Property(n => n.Value)
+                    .HasColumnName(nameof(Order.OrderName))
+                    .HasMaxLength(100)
+                    .IsRequired();
+                });
+            builder.ComplexProperty(
+                o => o.ShippingAddress, addresBuilder =>
+                {
+                    addresBuilder.Property(a => a.FirstName)
+                    .HasMaxLength(50)
+                    .IsRequired();
+
+                    addresBuilder.Property(a => a.LastName)
+                    .HasMaxLength(50)
+                    .IsRequired();
+
+                    addresBuilder.Property(a => a.EmailAddres)
+                    .HasMaxLength(50)
+                    .IsRequired();
+
+                    addresBuilder.Property(a => a.AddressLine)
+                    .HasMaxLength(180)
+                    .IsRequired();
+
+                    addresBuilder.Property(a => a.Country)
+                    .HasMaxLength(50)
+                    .IsRequired();
+
+                    addresBuilder.Property(a => a.State)
+                    .HasMaxLength(50)
+                    .IsRequired();
+
+                    addresBuilder.Property(a => a.ZipCode)
+                    .HasMaxLength(5)
+                    .IsRequired();
+                });
+            builder.ComplexProperty(
+                o => o.BillingAddress, addresBuilder =>
+                {
+                    addresBuilder.Property(a => a.FirstName)
+                    .HasMaxLength(50)
+                    .IsRequired();
+
+                    addresBuilder.Property(a => a.LastName)
+                    .HasMaxLength(50)
+                    .IsRequired();
+
+                    addresBuilder.Property(a => a.EmailAddres)
+                    .HasMaxLength(50)
+                    .IsRequired();
+
+                    addresBuilder.Property(a => a.AddressLine)
+                    .HasMaxLength(180)
+                    .IsRequired();
+
+                    addresBuilder.Property(a => a.Country)
+                    .HasMaxLength(50)
+                    .IsRequired();
+
+                    addresBuilder.Property(a => a.State)
+                    .HasMaxLength(50)
+                    .IsRequired();
+
+                    addresBuilder.Property(a => a.ZipCode)
+                    .HasMaxLength(5)
+                    .IsRequired();
+                });
+            builder.ComplexProperty(
+                o => o.Payment, paymentBuilder =>
+                {
+                    paymentBuilder.Property(a => a.CardNumber)
+                    .HasMaxLength(50)
+                    .IsRequired();
+
+                    paymentBuilder.Property(a => a.CardName)
+                    .HasMaxLength(24)
+                    .IsRequired();
+
+                    paymentBuilder.Property(a => a.Expiration)
+                    .HasMaxLength(10)
+                    .IsRequired();
+
+                    paymentBuilder.Property(a => a.CVV)
+                    .HasMaxLength(3)
+                    .IsRequired();
+
+                    paymentBuilder.Property(a => a.PaymentMethod);
+                });
+            builder.Property(o => o.Status)
+                .HasDefaultValue(OrderStatus.Draft)
+                .HasConversion(
+                    s => s.ToString(),
+                    dbStasus => (OrderStatus)Enum.Parse(typeof(OrderStatus), dbStasus));
+        }
+    }
+}
---
+++ scr/Services/Ordering/Ordering.Infrastructure/Data/Extensions/InitialData.cs
@@ -0,0 +1,57 @@
+﻿namespace Ordering.Infrastructure.Data.Extensions
+{
+    internal class InitialData
+    {
+        public static IEnumerable<Customer> Customers =>
+            new List<Customer>
+            {
+                Customer.Create(CustomerId.Of(new Guid("58cc4949-eec6-4de2-86e7-033c546291aa")),"Ahsan","ahsan@gmail.com"),
+                Customer.Create(CustomerId.Of(new Guid("58cc4949-eec6-4de2-86e7-033c546291ab")),"Ahmad","ahmad@gmail.com")
+            };
+
+        public static IEnumerable<Product> Products =>
+            new List<Product>
+            {
+                Product.Create(ProductId.Of(new Guid("78736c0c-b9a2-4b69-8075-8b0b62d9c8a7")),"Sumsong S12",100),
+                Product.Create(ProductId.Of(new Guid("0191dc0b-50c7-4b67-8a1b-8b1c6551d851")),"Sumsong F12",700),
+                Product.Create(ProductId.Of(new Guid("6eaa68ca-6f49-42ad-bb20-b160ea60cc82")),"Sumsong F102",100),
+                Product.Create(ProductId.Of(new Guid("0191dc08-0b01-4d69-a825-c9310d5b349a")),"Sumsong F13",700)
+            };
+
+        public static IEnumerable<Order> OrderandItems
+        {
+            get
+            {
+                var address1 = Address.Of("Ahsan", "Ullah", "ahsan@gmail.com", "Road 1", "Bangladesh", "Khulna", "9000");
+                var address2 = Address.Of("Ahmad", "Ullah", "ahmad@gmail.com", "Road 1", "Bangladesh", "Khulna", "9000");
+
+                var payment1 = Payment.Of("Ahsan", "5555555555555555", "12/28", "123", 1);
+                var payment2 = Payment.Of("Ahmad", "4444444444444444", "11/29", "234", 2);
+
+                var order1 = Order.Create(
+                    OrderId.Of(Guid.NewGuid()),
+                    CustomerId.Of(new Guid("58cc4949-eec6-4de2-86e7-033c546291aa")),
+                    OrderName.Of("Ord_1"),
+                    shippingAddress: address1,
+                    billingAddress: address1,
+                    payment1
+                    );
+                order1.Add(ProductId.Of(new Guid("78736c0c-b9a2-4b69-8075-8b0b62d9c8a7")),2, 100);
+                order1.Add(ProductId.Of(new Guid("0191dc0b-50c7-4b67-8a1b-8b1c6551d851")), 1, 700);
+
+                var order2 = Order.Create(
+                    OrderId.Of(Guid.NewGuid()),
+                    CustomerId.Of(new Guid("58cc4949-eec6-4de2-86e7-033c546291ab")),
+                    OrderName.Of("Ord_2"),
+                    shippingAddress: address2,
+                    billingAddress: address2,
+                    payment2
+                    );
+                order2.Add(ProductId.Of(new Guid("6eaa68ca-6f49-42ad-bb20-b160ea60cc82")), 2, 100);
+                order2.Add(ProductId.Of(new Guid("0191dc08-0b01-4d69-a825-c9310d5b349a")), 1, 700);
+
+                return new List<Order> { order1, order2 };
+            }
+        }
+    }
+}
