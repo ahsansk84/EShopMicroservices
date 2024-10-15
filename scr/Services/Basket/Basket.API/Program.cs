4e47b15cfd6a3390f43a0bd72740735927fd44c9
Ahsan Ullah Siddiquee <ahsansk84@hotmail.com>
2024-10-15 17:15:53 +0600

    Yeap with docker compose

--- scr/Services/Basket/Basket.API/Program.cs
+++ scr/Services/Basket/Basket.API/Program.cs
@@ -37,6 +37,15 @@ builder.Services.AddStackExchangeRedisCache(option =>
 builder.Services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>(option =>
 {
     option.Address = new Uri(builder.Configuration["GrpcSettings::DiscountUrl"]!);
+})
+.ConfigurePrimaryHttpMessageHandler(() =>
+{
+    var handler = new HttpClientHandler
+    {
+        ServerCertificateCustomValidationCallback =
+        HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
+    };
+    return handler;
 });
 
 //Async communication service
