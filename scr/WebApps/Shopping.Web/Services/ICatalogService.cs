52fee80516cadbb3c45740d623afad5ce6297070
Ahsan Ullah Siddiquee <ahsansk84@hotmail.com>
2024-10-16 20:14:42 +0600

    wed project

---
+++ scr/WebApps/Shopping.Web/Services/ICatalogService.cs
@@ -0,0 +1,15 @@
+﻿namespace Shopping.Web.Services
+{
+    public interface ICatalogService
+    {
+        [Get("/catalog-service/products?pageNumber={pageNumber}&pageSize={pageSize}")]
+        Task<GetProductsResponse> GetProducts(int? pageIndex = 1, int? pageSize = 10);
+        
+        [Get("/catalog-service/products/{id}")]
+        Task<GetProductsByIdResponse> GetProduct(Guid id);
+        
+        [Get("/catalog-service/products/category/{category}")]
+        Task<GetProductsByCategoryResponse> GetProductsByCategory(string category);
+    
+    }
+}
